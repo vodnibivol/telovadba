d7751853e0ecbc0f46530c5a16923a14082b4b4c
vodnibivol <vodnibivol@gmail.com>
2023-09-21 14:02:05 +0200

    deleted ServiceWorker options .. must correct

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service');
-const WorkboxPlugin = require('workbox-webpack-plugin');
+// const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = defineConfig({
   transpileDependencies: true,
@@ -14,33 +14,33 @@ module.exports = defineConfig({
     display: 'fullscreen',
 
     // custom SW
-    workboxPluginMode: 'InjectManifest',
-    workboxOptions: {
-      // ... other Workbox options
-      skipWaiting: true,
-    },
+    // workboxPluginMode: 'InjectManifest',
+    // workboxOptions: {
+    //   // ... other Workbox options
+    //   skipWaiting: true,
+    // },
   },
 
   // webpack service worker
-  configureWebpack: {
-    plugins: [
-      new WorkboxPlugin.GenerateSW({
-        // These options configure the behavior of the service worker.
-        clientsClaim: true,
-        skipWaiting: true,
-        // Define the static resources you want to precache here.
-        include: [
-          // /\.html$/,
-          // /\.js$/,
-          // /\.css$/,
-          // /\.png$/,
-          // /\.jpg$/,
-          // /\.svg$/,
-          /workout_./,
-          'https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600&display=swap',
-          'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
-        ],
-      }),
-    ],
-  },
+  // configureWebpack: {
+  //   plugins: [
+  //     new WorkboxPlugin.GenerateSW({
+  //       // These options configure the behavior of the service worker.
+  //       clientsClaim: true,
+  //       skipWaiting: true,
+  //       // Define the static resources you want to precache here.
+  //       include: [
+  //         // /\.html$/,
+  //         // /\.js$/,
+  //         // /\.css$/,
+  //         // /\.png$/,
+  //         // /\.jpg$/,
+  //         // /\.svg$/,
+  //         /workout_./,
+  //         // 'https://fonts.googleapis.com/css2?family=Lexend:wght@400;500;600&display=swap',
+  //         // 'https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200',
+  //       ],
+  //     }),
+  //   ],
+  // },
 });
